ec0390feeddfa1dc23df8ad4b820d226ec231710
alpha <georgeofori2005@gmail.com>
2026-04-06 03:31:21 -0700

    June 7, 2026

---
+++ components/index.js
@@ -0,0 +1,32 @@
+window.GenericComponent = {
+	props: ["type", "title", "description"],
+	template: `
+
+		<div class=type+"-container">
+		<h2>
+		<slot></slot>
+		<p class="title" v-text="title"></p>
+		</h2>
+
+		<div class="description" v-text="description"></div>
+		</div>
+	`,
+}
+
+window.ProjectCard = {
+	props: ["href", "name", "description", "skills_involved"],
+	template: `
+
+		<div class="project-container">
+		<a :href="href" class="site-name"><h3 v-text="name"></h3></a>
+		<p class="p-description" v-text="description"></p>
+		<div class="skills-used">
+		<p class="skills-label">Skills Used: </p>
+		
+		<ul class="skills">
+		<li class="skill" v-for="for skl in skills_involved" :key="skl" v-text="skl"></li>
+		</ul>
+		</div>
+		</div>
+		`
+}
