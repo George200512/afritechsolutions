2f65ff3d06db1fa2d536cb2513316140052b6694
alpha <georgeofori2005@gmail.com>
2026-06-13 19:04:18 -0700

    15 June, 2026

--- components/index.js
+++ components/index.js
@@ -1,22 +1,35 @@
 window.GenericComponent = {
 	props: ["type", "title", "description"],
+	data(){
+		return {
+			type,
+			title,
+			description
+		}
+	},
 	template: `
-
-		<div class=type+"-container">
+		<div class={{type+"-container"}}>
 		<h2>
 		<slot></slot>
-		<p class="title" v-text="title"></p>
+		<p class="ttl">{{title}}</p>
 		</h2>
 
-		<div class="description" v-text="description"></div>
+		<div class="descr">{{description}}</div>
 		</div>
 	`,
 }
 
 window.ProjectCard = {
 	props: ["href", "name", "description", "skills_involved"],
+	data() {
+		return {
+			href,
+			name,
+			description,
+			skills_involved
+		}
+	},
 	template: `
-
 		<div class="project-container">
 		<a :href="href" class="site-name"><h3 v-text="name"></h3></a>
 		<p class="p-description" v-text="description"></p>
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 const app = Vue.createApp(
 {
 	components: {
-		GenericComponent: window.GenericComponent,
-		ProjectCard: window.ProjectCard	
+		'GenericComponent': window.GenericComponent,
+		'ProjectCard': window.ProjectCard	
 	}
 }
 	);
 app.mount("#app");
+console.log(window.GenericComponent, window.ProjectCard)
