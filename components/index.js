8a3757777f9c4de8f3b6ac07a82cdafee06e02b7
alpha <georgeofori2005@gmail.com>
2026-06-24 17:40:24 -0700

    26 June, 2026

--- components/index.js
+++ components/index.js
@@ -16,13 +16,13 @@ window.ProjectCard = {
 	props: ["href", "name", "description", "skills_involved"],
 	template: `
 		<div class="project-container">
-		<a :href="href" class="site-name"><h3 v-text="name"></h3></a>
-		<p class="p-description" v-text="description"></p>
+		<a :href="href" class="site-name"><h3>{{name}}</h3></a>
+		<p class="p-description">{{description}}</p>
 		<div class="skills-used">
 		<p class="skills-label">Skills Used: </p>
 		
 		<ul class="skills">
-		<li class="skill" v-for="skl in skills_involved" :key="skl" v-text="skl"></li>
+		<li class="skill" v-for="skl in skills_involved" :key="skl">{{skl}}</li>
 		</ul>
 		</div>
 		</div>
